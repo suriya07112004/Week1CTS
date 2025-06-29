350f11c1621c51cda79896e157cf9582034dff93
Suriya071104 <suriya07112004@gmail.com>
2025-06-29 22:06:19 +0530

    add files

---
+++ WEEK 2/Ex 3 Assertions in JUnit/CODE/AssertionsTest.java
@@ -0,0 +1,16 @@
+package com.example.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class AssertionsTest {
+
+    @Test
+    public void testAssertions() {
+        assertEquals(5, 2 + 3);
+        assertTrue(5 > 3);
+        assertFalse(5 < 3);
+        assertNull(null);
+        assertNotNull(new Object());
+    }
+}
